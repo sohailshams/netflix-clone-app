924b3d887777e5deeef58cd9494f859e3a3bcccf
sohailshams <sohailshamas@yahoo.com>
2021-02-04 13:12:54 +0100

    Added styling to the nav - added nav__black class and added bg color black

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,7 @@ import './Nav.css';
 
 function Nav() {
     return (
-        <div className="nav">
+        <div className="nav nav__black">
             <div className="nav__contents">
                 <img className="nav__logo" src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt="netflix logo"/>
                 <img className="nav__avatar" src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png" alt="netflix avatar"/>
