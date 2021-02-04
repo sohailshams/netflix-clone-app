eedb7908482130ee79c741e2fedf555e90467dc2
sohailshams <sohailshamas@yahoo.com>
2021-02-04 15:50:47 +0100

    Added component Banner.js and Banner.css file and add it in app.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
 import React from 'react';
 import Home from './components/Home';
+import Banner from './components/Banner';
 import './App.css';
+
 
 function App() {
   return (
   <div className="app">
     <Home />
-    <h1>tesr</h1>
-    <h1>tesr</h1>
-    <h1>tesr</h1>
-    <h1>tesr</h1>
-    <h1>tesr</h1>
-    <h1>tesr</h1>
-    <h1>tesr</h1>
-    <h1>tesr</h1>
-    <h1>tesr</h1>
-    <h1>tesr</h1>
-    <h1>tesr</h1>
-    <h1>tesr</h1>
-    <h1>tesr</h1>
-    <h1>tesr</h1>
-    <h1>tesr</h1>
-    <h1>tesr</h1>
-    <h1>tesr</h1>
-    <h1>tesr</h1>
-    <h1>tesr</h1>
-    <h1>tesr</h1>
-    <h1>tesr</h1>
-    <h1>tesr</h1>
+    <Banner />
   </div>
   )
 }
