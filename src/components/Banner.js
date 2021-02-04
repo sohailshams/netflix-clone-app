b693486a385737226dd9604e289e98fa21d1a3d5
sohailshams <sohailshamas@yahoo.com>
2021-02-04 17:02:23 +0100

    Created a truncate function to limit the movie description to 150 character and put ... afterwards

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,6 +2,12 @@ import React from 'react';
 import './Banner.css';
 
 function Banner() {
+
+    function truncate(string, n) {
+        return string?.length > n ? string.substr(0, n - 1) + '...' : string;
+    }
+
+
   return (
     <header className="banner" style={{
         backgroundSize: "cover",
@@ -17,7 +23,7 @@ function Banner() {
                 <button className="banner__button">Play</button>
                 <button className="banner__button">My List</button>
             </div>
-             <h1 className="banner__description">This is test description of the movie</h1>
+             <h1 className="banner__description">{truncate(`This is test movie description.`, 150)}</h1>
         </div>
         <div className="banner--fadeBottom" />
     </header>
