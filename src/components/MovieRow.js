d0f28308c68ccef654b67cf5892f6a32a0e17e45
sohailshams <sohailshamas@yahoo.com>
2021-02-04 18:27:12 +0100

    Added movies state and added useEffect function to fetch movie

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from '../axios';
 import './MovieRow';
 
-function MovieRow({ title }) {
+function MovieRow({ title, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
   return (
     <div className="movierow">
       <h2>{title}</h2>
