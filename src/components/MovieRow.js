48b8f5bce48101810a2052b0bcc6ed94bd1a4c6e
sohailshams <sohailshamas@yahoo.com>
2021-02-04 18:46:30 +0100

    Added styling to show movies in a row and hide the horizontal scroll bar

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from '../axios';
-import './MovieRow';
+import './MovieRow.css';
 
 const base_url = 'https://image.tmdb.org/t/p/original/';
 
