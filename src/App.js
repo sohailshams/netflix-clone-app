b1a843b7255cfc4684ac9d7878d7fab8089db614
sohailshams <sohailshamas@yahoo.com>
2021-02-04 18:06:17 +0100

    Creaded MovieRow component, MovieRow.css - added MovieRow component in app.js and made bg black of the app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Home from './components/Home';
 import Banner from './components/Banner';
+import MovieRow from './components/MovieRow';
 import './App.css';
-
 
 function App() {
   return (
   <div className="app">
     <Home />
     <Banner />
+    <MovieRow />
   </div>
   )
 }
---
+++ src/components/MovieRow.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './MovieRow';
+
+function MovieRow() {
+  return <div className="movierow"></div>;
+}
+
+export default MovieRow;
