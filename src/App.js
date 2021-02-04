251204b0bfdd2ad950f5b3149ec8c6ddbec66099
sohailshams <sohailshamas@yahoo.com>
2021-02-04 19:36:04 +0100

    Updated file structure and installed firebase tools

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
 import React from 'react';
 import Home from './components/Home';
-import Banner from './components/Banner';
-import MovieRow from './components/MovieRow';
-import requests from './requests';
 import './App.css';
 
 function App() {
   return (
   <div className="app">
     <Home />
-    <Banner />
-    <MovieRow
-        title="NETFLIX ORIGINALS"
-        fetchUrl={requests.fetchNetflixOriginals}
-        isLargeRow
-      />
-      <MovieRow title="Trending Now" fetchUrl={requests.fetchTrending} />
-      <MovieRow title="Top Rated" fetchUrl={requests.fetchToRated} />
-      <MovieRow title="Action Movies" fetchUrl={requests.fetchActionMovies} />
-      <MovieRow title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
-      <MovieRow title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
-      <MovieRow
-        title="Romance Movies"
-        fetchUrl={requests.fetchRomanceMovies}
-      />
-      <MovieRow title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
   </div>
   )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import Banner from './Banner';
 import Nav from './Nav';
+import MovieRow from './MovieRow';
+import requests from '../requests';
 import './Home.css';
 
 function Home() {
   return (
     <div className="home">
       <Nav />
+      <Banner />
+      <MovieRow
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <MovieRow title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <MovieRow title="Top Rated" fetchUrl={requests.fetchToRated} />
+      <MovieRow title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <MovieRow title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <MovieRow title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <MovieRow title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <MovieRow title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
