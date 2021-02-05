2d7b0682da6cffe68a2846ff432c4a5ea00bcb23
sohailshams <sohailshamas@yahoo.com>
2021-02-05 19:04:12 +0100

    Added register and signIn function and added onClick function in sign button and sign up

--- src/components/screens/SignupScreen.js
+++ src/components/screens/SignupScreen.js
@@ -2,16 +2,28 @@ import React from 'react';
 import './SignupScreen.css';
 
 function SignupScreen() {
+  const register = (e) => {
+    e.preventDefault();
+  };
+
+  const signIn = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="signupScreen">
       <form>
         <h1>Sign In</h1>
         <input placeholder="Email" type="email" />
         <input placeholder="Password" type="password" />
-        <button type="submit">Sign In</button>
+        <button type="submit" onClick={signIn}>
+          Sign In
+        </button>
         <h4>
           <span className="signupScreen__grey">New to Netflix?</span>{' '}
-          <span className="signupScreen__link">Sign Up now.</span>
+          <span className="signupScreen__link" onClick={register}>
+            Sign Up now.
+          </span>
         </h4>
       </form>
     </div>
