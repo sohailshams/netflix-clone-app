dca7e8f549b5621bd99e8aeace3a7deaa16457ee
sohailshams <sohailshamas@yahoo.com>
2021-02-05 21:55:46 +0100

    Added useHistory hook and added onClick function on avatar to go to profile page

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,30 +1,41 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import './Nav.css';
 
 function Nav() {
-    const [show, handleShow] = useState(false);
+  const [show, handleShow] = useState(false);
+  const history = useHistory();
 
-    const transitionNavBar = () => {
-        if (window.scrollY > 100) {
-            handleShow(true);
-        } else {
-            handleShow(false);
-        }
+  const transitionNavBar = () => {
+    if (window.scrollY > 100) {
+      handleShow(true);
+    } else {
+      handleShow(false);
     }
+  };
 
-    useEffect(() => {
-        window.addEventListener("scroll", transitionNavBar);
-        return () =>  window.addEventListener("scroll", transitionNavBar);
-    }, [])
+  useEffect(() => {
+    window.addEventListener('scroll', transitionNavBar);
+    return () => window.addEventListener('scroll', transitionNavBar);
+  }, []);
 
-    return (
-        <div className={`nav ${ show && "nav__black"}`}>
-            <div className="nav__contents">
-                <img className="nav__logo" src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt="netflix logo"/>
-                <img className="nav__avatar" src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png" alt="netflix avatar"/>
-            </div>
-        </div>
-    )
+  return (
+    <div className={`nav ${show && 'nav__black'}`}>
+      <div className="nav__contents">
+        <img
+          className="nav__logo"
+          src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+          alt="netflix logo"
+        />
+        <img
+          onClick={() => history.push('/profile')}
+          className="nav__avatar"
+          src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
+          alt="netflix avatar"
+        />
+      </div>
+    </div>
+  );
 }
 
-export default Nav
+export default Nav;
