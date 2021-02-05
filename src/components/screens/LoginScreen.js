37b53670e9ba572ad940ced5796bc59dbfb19aa4
sohailshams <sohailshamas@yahoo.com>
2021-02-05 17:17:34 +0100

    Added text on the screen and its styling

--- src/components/screens/LoginScreen.js
+++ src/components/screens/LoginScreen.js
@@ -4,13 +4,20 @@ import './LoginScreen.css';
 
 function LoginScreen() {
   return (
-  <div className="loginScreen">
-      <div className="loginScreen__background">
-          <img className="loginScreen__logo" src={logo} alt="image"/>
-        <button className="loginScreen__button">Sign In</button>
-        <div className="loginScreen__gradient" />
-      </div>
-  </div>
+    <div className="loginScreen">
+        <div className="loginScreen__background">
+            <img className="loginScreen__logo" src={logo} alt="image"/>
+            <button className="loginScreen__button">Sign In</button>
+            <div className="loginScreen__gradient" />
+        </div>
+        <div className="loginScreen__body">
+          <h1>Unlimited films, TV programmes and more.</h1>
+          <h2>Watch anywhere. Cancel at any time.</h2>
+          <h3>Ready ro watch? Enter Your email to create or
+              restart membership.
+          </h3>
+        </div>
+    </div>
   );
 }
 
