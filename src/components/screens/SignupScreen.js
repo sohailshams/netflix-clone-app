16538c6611f4251968f0a602f630cbaa7aaa22a2
sohailshams <sohailshamas@yahoo.com>
2021-02-05 19:39:46 +0100

    Updated register and sigIn functions to create and signin user and checked it by console log

--- src/components/screens/SignupScreen.js
+++ src/components/screens/SignupScreen.js
@@ -1,4 +1,5 @@
 import React, { useRef } from 'react';
+import { auth } from '../../firebase';
 import './SignupScreen.css';
 
 function SignupScreen() {
@@ -7,10 +8,32 @@ function SignupScreen() {
 
   const register = (e) => {
     e.preventDefault();
+    auth
+      .createUserWithEmailAndPassword(
+        emailRef.current.value,
+        passwordRef.current.value
+      )
+      .then((authUser) => {
+        console.log(authUser);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
   };
 
   const signIn = (e) => {
     e.preventDefault();
+    auth
+      .signInWithEmailAndPassword(
+        emailRef.current.value,
+        passwordRef.current.value
+      )
+      .then((authUser) => {
+        console.log(authUser);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
   };
 
   return (
