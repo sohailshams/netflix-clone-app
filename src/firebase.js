a94fde4440e5c939593d42444fb6312e785072e9
sohailshams <sohailshamas@yahoo.com>
2021-02-05 12:56:41 +0100

    Added firebase.js and firebase configrations - added react-router-dom

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyBlX3n1oZKUyJOIvXhb0AzAPkg_kJ-66sY',
+  authDomain: 'netflix-clone-app-93fe9.firebaseapp.com',
+  projectId: 'netflix-clone-app-93fe9',
+  storageBucket: 'netflix-clone-app-93fe9.appspot.com',
+  messagingSenderId: '767016958827',
+  appId: '1:767016958827:web:12b600d71bb8d9e77baa2e',
+};
