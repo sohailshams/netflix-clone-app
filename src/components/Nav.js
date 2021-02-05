668b3ea6e6a6796f18d8a1fb60acf2a13278b8e4
sohailshams <sohailshamas@yahoo.com>
2021-02-05 22:01:52 +0100

    Added onClick function on netflix logo to go back to home screen

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -23,6 +23,7 @@ function Nav() {
     <div className={`nav ${show && 'nav__black'}`}>
       <div className="nav__contents">
         <img
+          onClick={() => history.push('/')}
           className="nav__logo"
           src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
           alt="netflix logo"
