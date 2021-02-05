31d07b20d3984f4f58a6af2571b88ef7c6543f3b
sohailshams <sohailshamas@yahoo.com>
2021-02-05 19:00:31 +0100

    Updated firebase.js - initialized the app and exported auth and db

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebse from 'firebase';
+
 const firebaseConfig = {
   apiKey: 'AIzaSyBlX3n1oZKUyJOIvXhb0AzAPkg_kJ-66sY',
   authDomain: 'netflix-clone-app-93fe9.firebaseapp.com',
@@ -6,3 +8,10 @@ const firebaseConfig = {
   messagingSenderId: '767016958827',
   appId: '1:767016958827:web:12b600d71bb8d9e77baa2e',
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebse.auth();
+
+export { auth };
+export default db;
