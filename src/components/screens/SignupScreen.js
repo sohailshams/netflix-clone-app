d79b92d2d911f106eb6d43a4ca7ad58b38989b87
sohailshams <sohailshamas@yahoo.com>
2021-02-05 18:32:12 +0100

    Added sign in button and password input field and styled the form

--- src/components/screens/SignupScreen.js
+++ src/components/screens/SignupScreen.js
@@ -7,6 +7,8 @@ function SignupScreen() {
       <form>
         <h1>Sign In</h1>
         <input placeholder="Email" type="email" />
+        <input placeholder="Password" type="password" />
+        <button type="submit">Sign In</button>
       </form>
     </div>
   );
