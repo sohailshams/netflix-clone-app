b975b3871b5d9d27e6157363fd9e0696f8962c37
sohailshams <sohailshamas@yahoo.com>
2021-02-06 15:21:40 +0100

    Added useEffect to fech subscriptions, adde loadcheckout to fire stripe checkout and added fucntion to determine which subscription is currently subscribed

--- src/components/screens/PlansScreen.js
+++ src/components/screens/PlansScreen.js
@@ -1,10 +1,34 @@
 import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { selectUser } from '../../features/userSlice';
 import db from '../../firebase';
+import {loadStripe} from '@stripe/stripe-js';
 import './PlansScreen.css';
 
 function PlansScreen() {
   const [products, setProducts] = useState([]);
+  const [subscription, setSubscription] = useState(null);
+  const user = useSelector(selectUser);
 
+  useEffect(() => {
+      db.collection('customers')
+      .doc(user.uid)
+      .collection('subscriptions')
+      .get()
+      .then((querySnapshot) => {
+          querySnapshot.forEach(async (subscription) => {
+            setSubscription({
+                role: subscription.data().role,
+                current_period_end: subscription.data().current_period_end.seconds,
+                current_period_start: subscription.data().current_period_start
+                  .seconds,
+              });
+          });
+      });
+
+  }, [user.uid])
+
+  
   useEffect(() => {
     db.collection('products')
       .where('active', '==', true)
@@ -25,18 +49,48 @@ function PlansScreen() {
       });
   }, []);
 
+  const loadCheckout = async (priceId) => {
+      const docRef = await db
+      .collection("customers")
+      .doc(user.uid)
+      .collection('checkout_sessions')
+      .add({
+          price: priceId,
+          success_url: window.location.origin,
+          cancel_url: window.location.origin,
+      });
+
+      docRef.onSnapshot(async (snap) => {
+          const {error, sessionId} = snap.data();
+
+          if (error) {
+          // Show error message to customer
+          // Inspect your cloud functions logs in the firebase console
+          alert(`An error occured: $(error.message)`);
+          }
+          if(sessionId) {
+            // We have the session, let's redirect to checkout
+            // Initialize Stripe
+
+            const stripe = await loadStripe("pk_test_51IHdg1GQx6MiAxr54DhrNbbnaVxLQtk8PBrPGwi56CyT2SpdMMVqfAQNn3a2HOewdrxe4urmnQnYwoTT6ACqvfJ400v9v8SxWE");
+            stripe.redirectToCheckout({sessionId});
+          }
+      });
+  };
   return (
     <div className="plansScreen">
         {Object.entries(products).map(([productId, productData]) => {
-            // Todo
+            
+            const isCurrentPackage = productData.name?.toLowerCase().includes(subscription?.role);
+            console.log(isCurrentPackage);
             return (
                 <div className="plansScreen__plan">
                     <div className="plansScreen__info">
                         <h5>{productData.name}</h5>
                         <h6>{productData.description}</h6>
                     </div>
-                    <button>
-                        Subscribe
+                    <button onClick={() => !isCurrentPackage && loadCheckout(productData?.prices?.priceId)}>
+                        {isCurrentPackage ? 'Current Package' : 'Subscribe'}
                     </button>
                 </div>
 
